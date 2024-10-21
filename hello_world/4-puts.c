52a5580fa105d3d8531c592908c99cacfebeb66f
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-21 14:03:23 +0000

    Amend 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main (void) 
+int main () 
 
 { 
-	puts ("Programming is like building a multilingual puzzle");
+	puts ("Programming is like building a multilingual puzzle);
 	puts ("");
-	
 
+	return 0;
 }
