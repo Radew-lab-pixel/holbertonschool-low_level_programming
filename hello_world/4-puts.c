58c4eeba7e20d4b15797b7d833ecaf034a0aaae2
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-21 21:47:12 +0000

    amend 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  * Description: \"Display Programming is like building a multilingual puzzle.
  */
 #include <stdio.h>
 
 /**
  * int main (void )  - main function that display the output
- * return (0) all the time 
+ * return (0) all the time
  */
 int main (void)
-{	
+{
 puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+return(0);
 }
