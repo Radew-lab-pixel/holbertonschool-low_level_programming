7e97b64e7382f89f20d6c5a371e4f78297ae9704
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-21 14:39:35 +0000

    Amend 4-puts.c - changes to holberton format

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- *Print output \"Programming is like building a multilingual puzzle
+ * main (void) - main function 
+ * Print output \"Programming is like building a multilingual puzzle
+ * return zero
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
