a02381a99c11bc5b6118495115c93199c1d2a8fd
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-21 23:03:30 +0000

    Amend 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 	printf("The total size of the int data type is equal to : %d\n",sizeof(int));
 	printf("The total size of char data type is equal to : %d\n",sizeof(char));
 	printf("The total size of the float data type is equal to : %d\n",sizeof(float));
-	printf("The total size of the double data type is equal to : %d\n",sizeof(double))
+	printf("The total size of the double data type is equal to : %d\n",sizeof(double));
 	return (0);
 }
