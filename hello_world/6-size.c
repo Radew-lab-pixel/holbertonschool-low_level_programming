b8579eb2f68b7534751dcd8695248cddb213f0f1
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-22 02:23:32 +0000

    Amend 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %u byte(s)\n", sizeof(a));
-	printf("Size of a int: %ud byte(s)\n", sizeof(b));
-	printf("Size of a long int: %u byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %u byte(s)\n", sizeof(d));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of a int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a float: %u byte(s)\n", sizeof(e));
 	return (0);
 }
