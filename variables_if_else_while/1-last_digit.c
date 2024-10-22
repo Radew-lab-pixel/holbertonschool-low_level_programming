4072874d4ca084f0f6f646488cf8d63d7b4b6741
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-22 07:06:06 +0000

    Amend 1-last_digit.c betty way

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,9 +26,13 @@ if (last_digit > 5)
 { printf("and is greater than 5\n");
 }
 else if (last_digit == 0)
-{ printf("and is 0");
+{ printf("and is 0\n");
 }
-else if (last_digit !=0 && last_digit < 6)
+/**
+else if (last_digit != 0 && last_digit < 6)
+{ printf(\"and is less than 6 and not 0\n\");
+}*/
+else
 { printf("and is less than 6 and not 0\n");
 }
 return (0);
