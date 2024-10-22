9397cf936d53044345dffdc401445043f7168c15
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-22 01:04:36 +0000

    Amend 5-printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,9 @@
-/**
- * Description: second c program
- */
-
 #include <stdio.h>
 
 /**
- * main-with proper grammar, but the outcome is a piece of art,
+ * main - Entry point
  *
- * Return: 0 Always
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
