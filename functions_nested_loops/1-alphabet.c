4342b5b62c56cfe1bfa3d1951ed7ad2bdbd35906
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-23 10:01:42 +0000

    Amend 1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,26 +3,20 @@
 /* more headers goes there */
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
- * Description: ' program that prints _putchar, followed by a new line'
+ * Description: print a to z
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char a = 'a';
-	while ( a < 'z')
-	{ 
+
+	while (a < 'z')
+	{
 	_putchar(a);
 	a++;
 	}
 	_putchar('\n');
-}
-
-int main(void)
-{
-print_alphabet();
-return (0);
 }
