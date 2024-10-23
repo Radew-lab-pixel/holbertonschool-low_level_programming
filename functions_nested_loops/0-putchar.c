73f0fb9d410c28c4c2117b7a84984a49b1bc67d4
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-23 06:14:09 +0000

    Amend 0-putchar - update main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "main.h"
+
 /* more headers goes there */
 
 /**
@@ -9,8 +11,6 @@
  * Return: Always 0 (Success)
  */
 
-#include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -18,28 +18,17 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
 
 int main(void)
 {
 	char c[] = "_putchar";
-      	int progress;
-	int i = 0;
+     	int i = 0;
+	
 	while ( i< 8 )
 	{
-        progress = _putchar(c[i]);
+        _putchar(c[i]);
 	i++;
 	}
-	progress =_putchar('\n');
-	if ( progress == 1)
-	{
+	_putchar('\n');
 	return (0);
-	}
-	else 
-	{
-	return (1);
-	}
 }
