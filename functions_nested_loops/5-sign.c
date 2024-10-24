a5e072d7fc1c06f50722bfadb2aed1e3be422065
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-24 12:36:48 +0000

    Amend 5-sign.c - betty issue

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,30 +1,27 @@
 #include "main.h"
 
-/* more headers goes there */
-
 /**
- * function print_sign - Entry point c
- *
- * Paramter n - entry point n
- *
- * Description: print the sign of the number
- *
- * Return: Always 1 ( > 0 ), 0 ( = 0 ) and -1 ( < 0 ) 
+ * print_sign - print the sign of the input n
+ * @n : First operand n
+ * Return: Always 1 ( n > 0 ), 0 ( n = 0 ) and -1 ( n < 0 )
+ * Example:
+ * print_sign(98) -> +1, 0
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
 	}
-	else 
-	{	
+	else
+	{	_putchar('-');
 		return (-1);
-	}	
-
-
+	}
 }
