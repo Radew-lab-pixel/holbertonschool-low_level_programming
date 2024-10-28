94b93bc32154379c151c5f0df748cd04cbd53e55
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-28 21:46:57 +0000

    Amend 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -15,13 +15,19 @@ void print_square(int size)
 	int i = 0;
 	int j = 0;
 
-	for (j = 0; j < size; j++) /* number of row loop */
+	if (size > 0)
 	{
-		for (i = 0; i < size; i++) /* number of space # loop */
+		for (j = 0; j < size; j++) /* number of row loop */
 		{
-			_putchar('#');
+			for (i = 0; i < size; i++) /* number of space # loop */
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
-
 }
