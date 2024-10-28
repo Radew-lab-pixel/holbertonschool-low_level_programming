dbf0c432da7b95270f31acaf5c7cc1526903d65b
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-28 13:11:24 +0000

    Add 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply input a and input b
+ * @a : integer input a
+ * @b : integer input b
+ *
+ * Return: integer value of multipled value of a and b
+ *
+ * Example : mul( 5,4)  -> 20
+ **/
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
