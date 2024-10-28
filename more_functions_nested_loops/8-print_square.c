d729151c00557c40e8ce9dee2f9158978672500e
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-28 21:43:14 +0000

    Add 8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print square n times
+ * @size  : integer input
+ *
+ * Return: none  ( Successful )
+ *a
+ *
+ * Example : print_square(2)
+ **/
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	for (j = 0; j < size; j++) /* number of row loop */
+	{
+		for (i = 0; i < size; i++) /* number of space # loop */
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
