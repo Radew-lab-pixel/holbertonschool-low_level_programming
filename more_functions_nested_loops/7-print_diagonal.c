c1887e0d7b1dcec8a1d1f49fcc94f29ea0269bdd
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-28 21:27:58 +0000

    Amend 7-print_diagnonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,20 +13,27 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++) /*n number of lines*/
+	if ( n > 0 ) /* go through the process only if n > 0*/
 	{
-		for (j = 0; j < n; j++) /*n number of the spacing*/
+		for (i = 0; i < n; i++) /*n number of lines*/
 		{
-			if (j == i) /*j line spacing counter is equal to number of line counter i*/
+			for (j = 0; j < n; j++) /*n number of the spacing*/
 			{
-				_putchar('\\');
-			}
-			else /*if j not reaching i counter, add space*/
-			{
-				_putchar(' ');
-			}
-		}
+				if (j == i) /*j line spacing counter is equal to number of line counter i*/
+				{
+					_putchar('\\');
+				}
+				else /*if j not reaching i counter, add space*/
+				{
+					_putchar(' ');
+				}
+			} /*end for*/
 
-		_putchar('\n'); /*add a new line*/
+			_putchar('\n'); /*add a new line*/
+		} /*end for*/
+	}
+	else /* if n < 0 , print a newline only */
+	{
+		_putchar('\n');
 	}
 }
