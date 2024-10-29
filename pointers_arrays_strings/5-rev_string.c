ce50c02177ec8040faece450cc8f8d3cc264494b
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-29 13:56:31 +0000

    Amend 5-rev_string.h

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * rev_string  - display and reverse  string by input
@@ -53,4 +54,16 @@ int _strlen(char *s)
 		i++;
 	}
 	return (i);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
