62e91c9bef53fac01e4a9b48b33a51a9e387bc26
Radew-lab-pixel <ktmqliu@gmail.com>
2024-10-29 01:34:17 +0000

    Add 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - first entry
+ * @void  : nil input
+ *
+ * Return: 0  ( Successful )
+ **
+ * Example:
+ **/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0) /* find multiple of 3 */
+		{
+			printf("Fizz");
+		}
+		if ((i % 5) == 0) /* find multiple of 5 */
+		{
+			printf("Buzz");
+		}
+		if (((i % 3) != 0) && ((i % 5) != 0)) /* if none multiple of 3 or 5 */
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
