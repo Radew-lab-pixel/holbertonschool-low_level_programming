60656db414e22e1580bcc92408c0ca57a7ac49c8
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-01 00:02:05 +0000

    Add 100-prime_factor.c

---
+++ 100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+/* 
+ * main - first entry 
+ * find prime number of 612852475143
+ *
+*/
+
+int main(void) 
+{
+
+	long int reference = 612852475143;
+	int count = 2;  /* skip 0 and 1 as they are number of interest */
+	int largest_prime;
+	int remainder; /* if 0, then its a prime , automous prime ? */
+
+	while (count < reference) 
+	{
+		if (reference % count) == 2
+	}
+		
+}
+
+
