7db2b572ea21d71b737fa3a44e38d232cdda386b
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-02 06:03:38 +0000

    Amend 7-leet.c - too many loop

--- pointers_arrays_strings/7-leet_backup.c
+++ pointers_arrays_strings/7-leet_backup.c
@@ -11,22 +11,40 @@ char _charToLeet(char a);
 
 char *leet(char *s)
 {
-	int count;
+	int length, count;
 	/* char result; */
 
-	/*length = _strlen(s); */
-	/*obtain of length of s */
+	length = _strlen(s); /* obtain of length of s */
 
 	/* count from 0 to length */
 
-	/*for (count = 0; count < length; count++)*/
-	for (count = 0; s[count] != '\0'; count++)
+	for (count = 0; count < length; count++)
 	{
 		s[count] = _charToLeet(s[count]);
 	}
 	return (s); /* return result */
 }
 
+/**
+ * _strlen - check the length of string
+ * function declaration performed in main.h
+ * @s  : char pointer s
+ *
+ * Return: integer value of length of *s
+ *
+ * Example : _strlen(aa)
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 /**
  *  _charToLeet - convert char to Leet style
