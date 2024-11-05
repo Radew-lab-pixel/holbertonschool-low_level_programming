294790aaed7b18ede43ec4ed093c3aebf72bdfb6
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-05 11:59:15 +0000

    Amend 3-strspn - added stop for mismatch

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,9 +2,10 @@
 #include <stddef.h>
 
 /**
- * _strspn - search for char and return the number
+ * _strspn - search for matching char in the intial word
+ * and return the number matched
  *
- * @s : string pointer s
+ * @s : string pointer s where intial matching word is stored
  * @accept : char containing chars to be matched
  *
  * Return: int of the number of matched character
@@ -15,13 +16,18 @@ unsigned int _strspn(char *s, char *accept)
 
 {	unsigned int counter = 0;
 	/* counter for matched characters */
+	unsigned int counter_ref = 0;
+	/*counter for reference is *s == *accept is achieved */
 
 	char *begin_accept = accept;
 	/* *accept pointer is only -> one direction thus store begin location */
 
 	while (*s != '\0') /* current *s not equal 0 or '\0' */
 	{
-		accept = begin_accept;
+		counter_ref = counter;
+		/* stored the matched char counter for reference */
+
+		accept = begin_accept; /* reset *accept address to [0] */
 
 		while (*accept != '\0') /* accept is not end of line */
 		{
@@ -29,8 +35,15 @@ unsigned int _strspn(char *s, char *accept)
 			{
 			counter++; /* increase counter value */
 			}
+
 			accept++; /* increase *accept address pointer by 1 */
 		}
+
+		if (counter == counter_ref) /* no change in counter meant no match */
+		{
+			return (counter); /* terminate and return the matched counter */
+		}
+
 		s++; /* increase *s address pointer by 1 */
 	}
 
