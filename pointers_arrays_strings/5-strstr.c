78d00774ba7affacd1d95261ac9e567a79412fe2
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-06 08:45:45 +0000

    Amend 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,11 +13,9 @@ char *_strstr(char *haystack, char *needle)
 	int count;
 	int match = 0;
 	/* match flag if haystack and needle matches */
-
 	int length = _strlen(needle);
 
 	/* length of needle */
-
 	if (length == 0)
 
 	/* failsafe if length is 0 */
@@ -39,29 +37,24 @@ char *_strstr(char *haystack, char *needle)
 			{
 				if (haystack[count] == needle[count])
 				{
-					count++;
-			/* increase both haystack and needle array position by 1 */
-					match = /1; 
-			/* reaffirm match is found */
+					count++; /* increase both haystack and needle array position by 1 */
+					match = 1; /* reaffirm match is found */
 				}
 				else	 /* there a mismatch along the search */
 				{
 					match = 0; /* match reset to 0 */
 				}
 			}
-
-		/* count and length the same while  match also is 1 */
-		/* entire needle string matches a word in haystack */
+/* count and length the same while  match also is 1 entire needle string matches a word in haystack */
 			if ((count == length) & (match == 1))
 			{
 				return (haystack);
 			}
 		}
 		haystack++;
-		/* progress to next char in haystack */
+/* progress to next char in haystack */
 	}
-	if (*haystack == '\0')
-		/* reach end of line of haystack */
+	if (*haystack == '\0') /* reach end of line of haystack */
 	{
 		return (0); /* return 0 - NULL */
 	}
