1802a013c1d41887d9867f572dab6c2640dc2ffb
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-08 05:28:20 +0000

    Amend 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 
 /**
- * main: main input 
- * @argc: no of argv[] or argument passed 
- * @argv: array of argument passed 
- * Return: EXIT_SUCCESS; 
+ * main - main input
+ * Description: Take command line input and output it
+ * @argc: no of argv[] or argument passed
+ * @argv: array of argument passed
+ * Return: EXIT_SUCCESS
  */
 
 int main(int argc, char **argv)
 {
-        while(argc--)
-                printf("%s\n", *argv++);
-        exit(EXIT_SUCCESS);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	exit(EXIT_SUCCESS);
 }
