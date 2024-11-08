fb2e60a02c705e263618166c768f45c5f203279e
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-08 05:51:16 +0000

    Amend 1-args.c - betty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,19 +4,20 @@
 
 /**
  * main - main entry
- * Description: take commandline argument and output and print number
- * @argc: number of argument 
+ *
+ * Description : take commandline argument and output and print number
+ * @argc: number of argument
  * @argv: array of arguments
- * EXIT: EXIT_SUCCESS (Always)
+ * Return: 0  (Always)
  */
 
-int main (int argc, char *argv[])
-{	/* **argv not empty */ 
+int main(int argc, char *argv[])
+{
+	/* **argv not empty */
 	if (**argv != '\0')
-	{	
+	{
 		/* print argc - 1 ( ignore argv[0] as it command */
 		printf("%d\n", argc - 1);
 	}
 	return (0);
 }
-
