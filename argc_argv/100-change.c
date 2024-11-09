fb047cf903da29e3442ab7bcdbf0b0fcf9ed1839
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-09 23:33:49 +0000

    Backup

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -4,6 +4,7 @@
 
 int check_argv(char *s);
 int _isalpha(int c);
+int cal_coins(int c, int *mint);
 
 /**
  * main - main entry
@@ -18,6 +19,8 @@ int _isalpha(int c);
 int main(int argc, char *argv[])
 {
 	int result, reading, argv_Alpha;
+
+	int mint = {25, 10, 5, 2 , 1};
 
 	result = 0;
 	argv++;
@@ -40,7 +43,8 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			result = result + reading;
+			/*result = result + reading;*/
+			result = cal_coins(reading, mint);
 		}
 		argc--;
 		argv++;
@@ -103,7 +107,32 @@ int _isalpha(int c)
 	return (0);
 }
 
-int cal_coins(int c, int *)
+/**
+ * cal_coins - find the number coins for the change
+ *
+ * @c : First int c for the amount
+ * @mint : array int for the coins value that minted 
+ * Return: anmount of coins
+ *
+ */
+
+int cal_coins(int c, int *mint)
 {
-	
+	int count, coin_num, coin_needed ;
+
+
+	int length = *(&mint + 1) - mint; /*  &mint[5] - &mint[0]; */
+
+	count = 0;
+	coin_num = 0;
+	coin_needed = 0;
+	while (count < length)
+	{
+		coin_num =  c / mint[count];
+		coin_needed = coin_needed + coin_num;
+		c = c - mint_count * coin_num;
+		count++; 
+	}
+	return (coin_needed);
+
 }
