1bfc08003032fba60ee1965a9e5af3933411b73c
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-10 23:49:24 +0000

    Amend 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,23 +4,30 @@ char *create_array(unsigned int size, char c);
 
 /**
  * create_array : create an array and initialize it with specific content
- * Description : create an array and intialise the content with c  
+ * Description : create an array and intialise the content with c
  * @size : size of the array
- * @c : char c that is content 
- * Return : char array 
+ * @c : char c that is content
+ * Return : char array or NULL if invalide
  */
 
 char *create_array(unsigned int size, char c) 
 {
 	unsigned int i = 0;
 
-	char *a = (char*) malloc (size * sizeof(char));
+	char *a = (char*) malloc(size * sizeof(char));
 
-	while (i < size)
+	if (size <=0)
 	{
-		a[i] = c; 
-		i++;
+		return (NULL);
 	}
+	else
+	{
+		while (i < size)
+		{
+			a[i] = c;
+			i++;
+		}
 
 	return (a);
+	}
 }
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 #endif
