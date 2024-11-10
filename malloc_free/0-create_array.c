311d03aa83815b0704c8726a09db4db4b526718a
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-10 23:56:05 +0000

    Amend 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,6 +15,17 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	char *a = (char*) malloc(size * sizeof(char));
+	
+	if (size <=0)
+	{
+		return (NULL);
+	}
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+      	
 
 	if (size <=0)
 	{
