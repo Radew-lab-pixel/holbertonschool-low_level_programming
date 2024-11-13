123fddb04b6f7d7aaadf45873e881f3ce075ed4e
Radew-lab-pixel <ktmqliu@gamil.com>
2024-11-13 14:05:59 +0000

    Amend dog.h- bettu

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h> 
+#include <stdio.h>
 
-struct dog 
-{ 
+
+/**
+ * struct dog - create structure for three elements
+ *
+ * @name: char array
+ * @age: float
+ * @owner: char array
+ *
+ * Description: create stuct dog with three elements
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
