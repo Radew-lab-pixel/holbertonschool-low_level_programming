5b0e701eb386a13bd87984b1105736ac24f38ab8
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-14 00:02:37 +0000

    Backup

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,8 @@
+#include "dog.h"
+
+/**
+ * print_dog - to print stuct elements
+ * struct dog - struct of datatype dog
+ * Decription: function to create struct dog 
+
+void print_dog(struct dog *d)
