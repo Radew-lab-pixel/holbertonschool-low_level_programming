1987b007ea956ff6a99be3507ba3fdddcdb78f99
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-14 00:13:49 +0000

    Backup

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,6 +3,13 @@
 /**
  * print_dog - to print stuct elements
  * struct dog - struct of datatype dog
- * Decription: function to create struct dog 
+ * Decription: function to create struct dog
+ * @dog: struct input entry
+ * @d: struct array d
+ * Return: void 
+ */
 
 void print_dog(struct dog *d)
+{
+	struc
+}
