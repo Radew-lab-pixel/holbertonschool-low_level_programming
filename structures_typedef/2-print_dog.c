71ce220a1a93905e9757b102faf154bf89125910
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-14 01:00:32 +0000

    Ammend 2-print_dog.c - add null check

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,6 +16,20 @@ void print_dog(struct dog *d)
 
 	dest = d; /*copy d to dest */
 
+	if (dest->name == NULL)
+	{
+		dest->name = "(null)";
+	}
+	/* if (dest->age == NULL) float variable can't be null */
+	/*{*/
+	/*	dest->age = 0;*/
+	/*}*/
+	if (dest->owner == NULL)
+	{
+		dest->owner = "(null)";
+	}
+
+
 	printf("Name: %s\nAge: %.6f\n", dest->name, dest->age);
 	printf("Owner: %s\n", (*dest).owner);
 	return;
