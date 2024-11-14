c15c57182e04f5e7068b4184d163d9a50aba5a0f
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-14 00:39:24 +0000

    Add 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,14 +2,22 @@
 
 /**
  * print_dog - to print stuct elements
- * struct dog - struct of datatype dog
- * Decription: function to create struct dog
- * @dog: struct input entry
- * @d: struct array d
- * Return: void 
+ * struct dog - struct of datatype
+ *
+ * Description: function to create struct nameddog
+ *
+ * @d: object
+ * Return: void
  */
 
 void print_dog(struct dog *d)
 {
-	struc
+	struct dog *dest;
+
+	dest = d; /*copy d to dest */
+
+	printf("Name: %s\nAge: %.6f\n", dest->name, dest->age);
+	printf("Owner: %s\n", *dest.owner);
+	return;
+
 }
