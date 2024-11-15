a8fdb04bb7de06ed32870fbec18380e5f490338c
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-15 01:44:28 +0000

    Amend 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -18,10 +18,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dest;
 
 	dest = (dog_t *) malloc(sizeof(dog_t));
+	if (dest == NULL)
+	{
+		free(dest);
+		return (NULL);
+	}
 
 	/* age is float thus can't be NULL */
 	if ((name  == NULL) || (owner == NULL))
-	{
+	{	
+		/*free(dest->name);*/
+		/*free((*dest).age[0]);*/
+		/*free(dest->owner);*/
+		free(dest);
 		return (NULL);
 	}
 
