027cde883d9e681e74551b80300a0ab5f7fbb05b
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-20 03:07:18 +0000

    Backup

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,9 +12,10 @@
 
 int main(int argc, char *agrv[])
 {
-	char *tagrv;
-	va_list args; /* list of variadic lists */
-	va_start(args, **tagrv); /* enabled variadic access */
+	int result, a, b;
+	char *op; /* operand*/
+	/*va_list args; list of variadic lists */
+	/* va_start(args, **tagrv);  enabled variadic access */
 
 	if (argc < 4) /* less than 4 arguments */
 	{
@@ -23,7 +24,18 @@ int main(int argc, char *agrv[])
 	/* argv[1] and argv[3] are digit */
 	if (((isdigit(argv[1])) != 0) && ((isdigit(argv[3])) != 0))
 	{
-		op
+		
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		op = argv[2];
+		/*int (*get_op_func(char *s))(int, int);*/
+		result = get_op_func(op, a, b);
+		printf("%d\n",result);
+		return (0); 
+	}
+	else
+	{
+		return (NULL);
 	}
 
 	
