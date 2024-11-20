1c09b4ccc902dfa1643aa23eb520a3b69ebbf406
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-20 04:44:55 +0000

    Amend Task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
  * op_add - add a and b
@@ -45,6 +46,11 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{	
+		printf("Error\n");
+		exit(100);
+	}	
 	return (a / b);
 }
 
@@ -56,6 +62,11 @@ int op_div(int a, int b)
  */
 
 int op_mod(int a, int b)
-{
+{	
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b); 
 }
