8b832a3cd315c3ea033dc32765e2665180b14427
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-20 12:06:13 +0000

    Amend Task 3 - betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,40 +5,40 @@
  * get_op_func - search for respective function
  *
  * @s: char s for operator
- * @int: call back function with two required integer variables
- * @Return: the processed integer variable by call back function
+ * Description : integer passed in  call back function
+ * Return: pointer to function
  */
- 
+
 int (*get_op_func(char *s))(int, int)
 {
 
 	op_t ops[] = {
-		{"+", op_add},
+		{"+", op_adda,
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	};
-    	int i = 0;
+		};
+	int i = 0;
 	/*char *ts;  copy of the s */
 
 	/* typedef struct op */
 	/*{ */
-    	/* char *op; */
-    	/* int (*f)(int a, int b); */
+	/* char *op; */
+	/* int (*f)(int a, int b); */
 	/*} op_t; */
-	
+
 	while (i < 5)
 	{
 		/*if (ops[i].op == ts)*/
-		if ((strcmp (ops[i].op, s)) == 0) /* matched found */
+		if ((strcmp(ops[i].op, s)) == 0) /* matched found */
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
 	printf("Error\n");
-	exit (99);
+	exit(99);
 	return (ops[5].f);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -5,10 +5,10 @@
 /**
  * main - main entry
  *
- * @argc - no. of argument passed
- * @argv - array of arguments passed
+ * @argc: no. of argument passed
+ * @argv: array of arguments passed
  *
- * Return : 0 if successful
+ * Return: 0 if successful
  */
 
 int main(int argc, char *argv[])
@@ -23,20 +23,21 @@ int main(int argc, char *argv[])
 		/*return (NULL)*/
 		/*return (0);*/
 		printf("Error\n");
-		exit(98);	
+		exit(98);
 	}
 	/* argv[1] and argv[3] are digit */
-	/* if i use isdigit , it causes segmentation error as too many isdigit in if loop*/
+	/* if i use isdigit , it causes segmentation error*/
+	/*as too many isdigit in if loop*/
 	/*if (((isdigit(argv[1])) != 0) && ((isdigit(argv[3])) != 0)) */
 	/*{ */
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	/*op = *(argv[2]);*/
-	op = (char*) argv[2];
+	op = (char *) argv[2];
 	/*int (*get_op_func(char *s))(int, int);*/
 	result = get_op_func(op)(num1, num2);
-	printf("%d\n",result);
-	return (0); 	
+	printf("%d\n", result);
+	return (0);
 	/*return (NULL); */
 	/*return (0); */
 }
