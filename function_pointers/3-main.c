387cf9212a9c68e369dc8584c3614e8a32a17fe7
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-20 04:13:13 +0000

    Add Task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,26 +13,27 @@
 
 int main(int argc, char *argv[])
 {
-	int result, a, b;
+	int result, num1, num2;
 	char *op; /* operand*/
 	/*va_list args; list of variadic lists */
 	/* va_start(args, **tagrv);  enabled variadic access */
 
 	if (argc < 4) /* less than 4 arguments */
 	{
-		/*return (NULL);*/
-	       return (0);	
+		/*return (NULL)*/
+		/*return (0);*/
+		exit(98);	
 	}
 	/* argv[1] and argv[3] are digit */
 	/* if i use isdigit , it causes segmentation error as too many isdigit in if loop*/
 	/*if (((isdigit(argv[1])) != 0) && ((isdigit(argv[3])) != 0)) */
 	/*{ */
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	/*op = *(argv[2]);*/
 	op = (char*) argv[2];
 	/*int (*get_op_func(char *s))(int, int);*/
-	result = get_op_func(op)(a, b);
+	result = get_op_func(op)(num1, num2);
 	printf("%d\n",result);
 	return (0); 	
 	/*return (NULL); */
