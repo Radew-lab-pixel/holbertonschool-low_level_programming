4bdbfbe9c4d66b810a5715d7c2b8faea3e36b1eb
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-21 08:46:27 +0000

    Backup

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,4 +1,27 @@
 #include "variadic_functions.h"
 
+/**
+ * print_all - print all argument
+ *
+ * @format : format operand that determine the datatype
+ * unable to change content and address of format
+ * Return: void
+ */
 
 void print_all(const char * const format, ...)
+{
+    	/*char *form; */
+        /*void (*f)(va_list);*/
+	
+	op_t ops[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+		};
+
+
+}
+
+
