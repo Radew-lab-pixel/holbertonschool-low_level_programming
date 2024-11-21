0ce5666d840bc34556c9f7000cc78acfbc002f0f
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-21 02:11:58 +0000

    Add 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,3 +9,31 @@
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count = 0;
+	char *result;
+
+	/*Declare a variable type va_list*/
+	va_list argptr;
+	
+	/* Initialize the va variable argptr  where n is detected no. */
+	va_start(argptr, n);
+	
+	while (count < n)
+	{	
+		/* get the next addition*/
+		result = va_arg(argptr, char*);
+		printf("%s", result);
+
+		if (count != (n - 1)) /* not end of argument*/
+		{
+			printf("%s", separator);
+		}
+		else
+		{
+			printf("\n");
+		}
+	count++;
+	}
+	va_end(argptr); /* end use of va variable argptr */
+}
