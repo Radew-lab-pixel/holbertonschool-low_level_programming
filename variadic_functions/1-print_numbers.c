ada86ca3a0aa4e07d954fbaf89087d1f2d72c9b9
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-21 01:08:57 +0000

    Add 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,38 @@
 #include "variadic_functions.h"
 
-
+/**
+ * print_numbers - function to process variadic arguments
+ * @separator : seperator identifier
+ * @n : detected no. of integer variable
+ * Return: void
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	
+	unsigned int count, result;
 
+	/* Declare a variable of type va_list. */
+	va_list argptr;
+
+	/* Initialize that variable */
+	va_start(argptr, n);
+
+	for (count = 0; count < n; count++)
+	{
+		/* Get the next additional argument argptr. */
+		result = va_arg(argptr, int);
+		printf("%u", result);
+
+		if (count != (n - 1)) /*not last argumet argptr */
+		{
+			printf("%s", separator);
+		}
+		else /*last argument argptr */
+		{
+			printf("\n");
+		}
+	}
+	/* End use of the argptr variable. */
+	va_end(argptr);
+	/*return;  not needed for void return */
 }
