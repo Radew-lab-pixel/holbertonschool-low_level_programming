ac96f37ba03d6ab958f3e41bfe74cd0c7eb30026
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-21 08:34:53 +0000

    Backup

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,6 +5,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/*
+ * struct ops - Struct ops
+ * @form : format operand
+ * @f : the callback function associated
+ */
+
+typedef struct ops
+{
+	char *form;
+	void (*f)(va_list);
+} op_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
