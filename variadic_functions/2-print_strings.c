394929835856040d9544e74ce560dd977afc444d
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-21 01:53:56 +0000

    Backu[

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function to process argument and print its content
+ *
+ * @separator : separator input
+ * @n : no . of arguments
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
