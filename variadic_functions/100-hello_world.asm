2d65cb71581dff826e2309ec4faf66becaae8098
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-24 03:07:10 +0000

    Backup

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,3 +1,31 @@
 #include <unistd.h>
+#include <stdio.h>
 
+int _strlen(char *s);
 
+/**
+* main - main entry 
+* @void : void input
+* Return: int
+*/
+
+int main(void) 
+{
+	int length;
+	char *buffer = "Hello World\n"; 
+	
+	length = _strlen(buffer);
+	write(1,buffer,length);
+	return (0);
+	
+}
+
+int _strlen(char *s)
+{
+	int count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
