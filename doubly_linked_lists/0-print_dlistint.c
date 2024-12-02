132700d023645d61d3b09ec6adbc84911db8afa2
root <ktmqliu@gmail.com>
2024-12-02 22:46:58 +0000

    Add 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,3 +8,26 @@
  */
 
 size_t print_dlistint(const dlistint_t *h)
+{	
+	size_t count = 0;
+	/*const dlistint_t *temp;  create a temp file */
+	/* const dlistint_t typecast issue and also valgrind issue */	
+
+	if (h == NULL) /* error checking for h */
+	{
+		return (0);
+	}
+	
+	/* temp = *h; */
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	
+	/* display the last node */
+	printf("%d\n", h->n);
+	count++;
+	return (count);
+}
