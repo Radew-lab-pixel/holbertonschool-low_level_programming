a607149ccc4d44973d1dc7a243d2d69f1495395b
root <ktmqliu@gmail.com>
2024-12-02 10:32:55 +0000

    Amend 4-free_list.c - valgrind

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -21,15 +21,20 @@ void free_list(list_t *head)
 		/*tmp = head; */
 		while (head->next != NULL)
 		{	
-			temp = head;
-			head = head->next;
-			free(temp->str);
+			/*temp = head;*/
+			/*head = head->next; */
+			temp = head->next;
+			free(head->str);
+			free(head);
+			head = temp;
+			/*free(temp->str);*/
 			/* removed below due to segmentation error */
 			/*free(temp->next);*/
 			/*free(temp->len);*/
-			free(temp);
+			/*free(temp);*/
 
 		}
+
 		free(head);
 		return;
 	}
