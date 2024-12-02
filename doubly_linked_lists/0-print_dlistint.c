8a7590039c7dd16c18ed3f564c02cb2066f4e540
root <ktmqliu@gmail.com>
2024-12-02 22:49:32 +0000

    Amend 0-print_dlistint.c - betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 
 /**
- * print_dlistin - function that prints all the elements of a dlistint_t list
+ * print_dlistint - function that prints all the elements of a dlistint_t list
  *
  * @h : list input
  * Return: number of elements(nodes)
  */
 
 size_t print_dlistint(const dlistint_t *h)
-{	
+{
 	size_t count = 0;
 	/*const dlistint_t *temp;  create a temp file */
-	/* const dlistint_t typecast issue and also valgrind issue */	
+	/* const dlistint_t typecast issue and also valgrind issue */
 
 	if (h == NULL) /* error checking for h */
 	{
 		return (0);
 	}
-	
+
 	/* temp = *h; */
 	while (h->next != NULL)
 	{
@@ -25,7 +25,7 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 		h = h->next;
 	}
-	
+
 	/* display the last node */
 	printf("%d\n", h->n);
 	count++;
