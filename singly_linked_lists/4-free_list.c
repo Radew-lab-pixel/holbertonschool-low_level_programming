7de396db1c66bdbd7c8747ac5ce1387f35aff1d6
root <ktmqliu@gmail.com>
2024-12-02 10:13:27 +0000

    Amend 4-free_list.c - valgrind

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,14 +9,24 @@
 
 void free_list(list_t *head)
 {
+	list_t *temp;
+
 	if (head == NULL)
 	{
 		free(head);
 		return;
 	}
 	else
-	{
-		free(head->str);
+	{	
+		/*tmp = head; */
+		while (head->next != NULL)
+		{	
+			temp = head;
+			head = head->next;
+			free(temp->str);
+			free(temp);
+
+		}
 		free(head);
 		return;
 	}
