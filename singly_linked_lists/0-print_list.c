c8b38f535a48001adcdb69acfcc55f629606778c
root <ktmqliu@gmail.com>
2024-12-02 00:53:29 +0000

    Backup

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,116 @@
+#include "lists.h"
+#include <unistd.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+char *_strcpy(char *destination, char *source);
+void _puts(char *s);
+
+/**
+ * print_list -  function that prints all the elements of a list_t list
+ *
+ * @h : structure of list_t
+ * Return: length of h->str which is hcopy->len
+ */
+
+size_t print_list(const list_t *h)
+{
+	/*create list_t *hcopy;*/
+	list_t *hcopy = (list_t *)malloc(sizeof(list_t));
+
+	/*hcopy = h; keep a copy of the original list_t *h */
+	/*_strcpy(hcopy->str, h->str);*/
+	
+	/** hcopy->str = h->str;
+	*hcopy->len = h->len;
+	*hcopy->next = h->next;
+	*/
+	*hcopy = *h;
+
+	if (h->str == NULL)
+	{
+		/*hcopy->len = 0;*/
+		_strcpy(hcopy->str, "(nil)");
+		hcopy->len = 0;
+		/*return (0);*/
+	}
+	
+	while (hcopy->next != NULL)
+	{
+		_putchar('[');
+		_putchar(hcopy->len);
+		_putchar('] ');
+
+	}
+	return (hcopy->len);
+		
+}
+
+/**
+ * _putchar - custom putchar function to print a character
+ *
+ * @c : character to be printed 
+ * Return: 1 (Successful) else 0 (fail)
+ */
+
+int _putchar(char c)
+{
+	return (write(1,&c,1));
+}
+
+/**
+ * _strlen - custom strlen function to count character in string
+ *
+ * @s : string input
+ * Return: (count)
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (s[0] != '\0')
+	{
+		count++;
+		/*s++;*/
+	}
+	return (count);
+}
+
+/**
+ * _strcpy - custom strcpy function to copy two strings
+ *
+ * @destination : string pointer to be copied to
+ * @soruce : string point to be copied from
+ * Return: string pointer destination
+ */
+
+char *_strcpy(char *destination, char *source)
+{
+	
+	int length;
+	int count = 0;
+	
+	length = _strlen(source); /* length of source */
+	while (count < length)
+	{
+		destination[count] = source[count];
+		count++;
+	}
+	destination[count] = '\0';
+	return (destination);
+}
+
+
+void _puts(char *s)
+{
+	int count = 0;
+	int length = _strlen(s);
+
+	while (count < length)
+
+}
