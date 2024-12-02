9d95296be4253f3b71f6e43c40b5b6a38cb90da9
root <ktmqliu@gmail.com>
2024-12-02 22:30:21 +0000

    Backup

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_dlistin - function that prints all the elements of a dlistint_t list
+ *
+ * @h : list input
+ * Return: number of elements(nodes)
+ */
+
+size_t print_dlistint(const dlistint_t *h)
