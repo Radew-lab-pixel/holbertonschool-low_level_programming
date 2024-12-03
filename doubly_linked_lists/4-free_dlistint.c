72633608951f373cd7d6b0b872fc0319c5e41d96
root <ktmqliu@gmail.com>
2024-12-03 03:35:22 +0000

    Backup

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function to free memory of dlistint list
+ *
+ * @head : list reference
+ * Return: void if successful
+ */
+
+void free_dlistint(dlistint_t *head)
+
