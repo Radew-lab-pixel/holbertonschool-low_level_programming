e27977138b1dc69a80a962bc89497a50ef0bee01
root <ktmqliu@gmail.com>
2024-12-03 06:32:16 +0000

    Backup

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_t *insert_dnodeint_at_index - function to add new node at give position
+ * @h : lists
+ * @idx : position of the new node
+ * @n : value of the new note
+ *
+ * Return: address of new node else NULL if fails
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlist_t));
+	dlistint t *temp; /* local pointer to **h */
+
+
+}
