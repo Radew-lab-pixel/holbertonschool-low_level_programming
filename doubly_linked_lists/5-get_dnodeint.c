a2bdbaf7a7bd05a1c805fa6262cc3c31a1f4f5ab
root <ktmqliu@gmail.com>
2024-12-03 04:09:13 +0000

    Backup

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index = function to return certain node of the list
+ *
+ * @head : the list
+ * @index : the index of the node required
+ * Return: node required
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+}
