e4f665b6b037780f60f86c42731fc402009ee4de
root <ktmqliu@gmail.com>
2024-12-03 10:09:39 +0000

    Backup

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/*
+ * delete_dnodeint_at_index - delete node at certain index
+ *
+ * @head : list of nodes
+ * @index : index of the node to be deleted
+ * Return: 1 if successful else -1
+ */
+ 
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
