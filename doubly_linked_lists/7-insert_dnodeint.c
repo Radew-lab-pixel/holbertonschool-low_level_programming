d47cbda053da880db295a86501b19b9f6419d3e4
root <ktmqliu@gmail.com>
2024-12-03 08:05:11 +0000

    Amend 7-insert_dnodeint.c - add node insert at start and end

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -38,9 +38,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp = *h; /* pointer to *h */ 
 	while (count < idx)
 	{	
-		if (temp == NULL) /* end of list or NULL */
+		/*if (temp ==  NULL) end of list or NULL */
+		if (temp->next == NULL) /* last node in list */
 		{
-			return (NULL);
+			new_node = add_dnodeint_end(h, n);
+			return (new_node);
+			/*return (NULL);*/
 		}
 		else
 		{
