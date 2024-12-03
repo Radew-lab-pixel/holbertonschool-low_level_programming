47c4ebb751b04f26e12508d4478b6b944378f45c
root <ktmqliu@gmail.com>
2024-12-03 03:54:32 +0000

    Add 4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -4,8 +4,30 @@
  * free_dlistint - function to free memory of dlistint list
  *
  * @head : list reference
- * Return: void if successful
+ * Return: void always
  */
 
 void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
 
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+	while (temp->next != NULL)
+	{
+		temp = head->next; /*point to head->next */
+		/*free(head->next); */
+		/*free(head->prev); */
+		free(head); /* free current head pointer */
+		head = temp; /* reassign head to point to temp */
+	}
+	/* free the last node of the list */
+	/* free ->next and prev will caused segmentation or core dump error */
+	/*free(head->next);*/
+	/*free(head->prev);*/
+	free(head);
+	/*return; removed due to betty*/
+}
