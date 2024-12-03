e6adfeafca6e021f36d72bda5331b6e1121d1b37
root <ktmqliu@gmail.com>
2024-12-03 07:46:42 +0000

    8-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint_ver1.c
+++ doubly_linked_lists/7-insert_dnodeint_ver1.c
@@ -30,39 +30,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	temp = *h; /* pointer to *h */
 
-	while (count < idx)
-	{	
-		if (temp == NULL) /* end of list or NULL */
-		{
-			return (NULL);
-		}
-		else
-		{
-			prev_temp = temp;
-			temp = temp->next;
-			count++;
-		}
+	while ((temp->next != NULL) && (count < idx))
+	{
+		prev_temp = temp;
+		temp = temp->next;
+		count++;
 	}
 	if (count > idx)
 	{
 		return (NULL);
 	}	
-	
-	prev_temp->next = new_node;
-	new_node->prev = prev_temp;
-	
-	temp->prev = new_node;
-	new_node->next = temp;
-
-
-	/**
-	**temp->next = new_node;
-	**new_node->prev = prev_temp;  new_node->prev points to prev_temp */
-	
+	temp->next = new_node;
+	new_node->prev = prev_temp; /* new_node->prev points to prev_temp */
 	/** go to next address of temp */
-	/**temp = temp->next;
-	*temp->prev = new_node;  next address of temp->prev now point to new_node */
-	/*new_node->next = temp; */
+	temp = temp->next;
+	temp->prev = new_node; /* next address of temp->prev now point to new_node */
+	new_node->next = temp;
 
 	return (new_node);
 
