8ce723d516671cba8de173fc202de5fe77f0e254
root <ktmqliu@gmail.com>
2024-12-04 10:30:33 +0000

    Backup

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,53 +1,27 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node at certain index
+ * delete_dnodeint_at_index - function to delete a node
  *
- * @head : list of nodes
- * @index : index of the node to be deleted
- * Return: 1 if successful else -1
+ * @head : lists
+ * @index : location of node to be removed
+ *
+ * Return: 1 (successful) or -1 (fail)
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp;
 	dlistint_t *prev_temp;
-	dlistint_t *post_temp;
 	unsigned int count = 0;
 
-	if (*head == NULL) /* added for checker */
+	temp = *head;
+	while ((count < index) && (temp->next != NULL))
 	{
-		free(*head);
-		return (-1);
-	}
-	temp = *head; /* temp point to *head */
-	if (index == 0)  /* node to be deleted at start */
-	{
-		if (temp->next != NULL)
-		{
-			*head = (*head)->next;
-			(*head)->prev = NULL;
-		}
-		else
-			*head = NULL; /* instead of free(temp) or free(*head)*/
-		free(temp); /*added for valgrind */
-		return (1);
-	}
-	while ((count < index) && (temp != NULL))
-	{
-		if (temp->next == NULL)
-			return (-1); /* index is larger the actual no. of nodes */
-		prev_temp = temp; /* prev_temp to point to current temp */
-		temp = temp->next; /* temp point to next address */
+		prev_temp = temp; 
+		temp = temp->next;
 		count++;
 	}
-	if ((count != index) && (temp == NULL)) /* for neg value detection */
-		return (-1);
-	if (index > count) /* index required is greater than no, of nodes */
-		return (-1);
-	post_temp = temp->next; /* go next address after temp */
-	post_temp->prev = prev_temp; /* post_temp->prev ppint to prev_temp*/
-	prev_temp->next = post_temp; /* prev_temp-> next point to post_temp */
-	free(temp);
-	return (1); /*successful*/
-}
+
+
+
---
+++ doubly_linked_lists/8-delete_dnodeint_ver6_working_but_not_with_neg.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at certain index
+ *
+ * @head : list of nodes
+ * @index : index of the node to be deleted
+ * Return: 1 if successful else -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	dlistint_t *prev_temp;
+	dlistint_t *post_temp;
+	unsigned int count = 0;
+
+	if (*head == NULL) /* added for checker */
+	{
+		free(*head);
+		return (-1);
+	}
+	temp = *head; /* temp point to *head */
+	if (index == 0)  /* node to be deleted at start */
+	{
+		if (temp->next != NULL)
+		{
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+		}
+		else
+			*head = NULL; /* instead of free(temp) or free(*head)*/
+		free(temp); /*added for valgrind */
+		return (1);
+	}
+	while ((count < index) && (temp != NULL))
+	{
+		if (temp->next == NULL)
+			return (-1); /* index is larger the actual no. of nodes */
+		prev_temp = temp; /* prev_temp to point to current temp */
+		temp = temp->next; /* temp point to next address */
+		count++;
+	}
+	if ((count != index) && (temp == NULL)) /* for neg value detection */
+		return (-1);
+	if (index > count) /* index required is greater than no, of nodes */
+		return (-1);
+	post_temp = temp->next; /* go next address after temp */
+	post_temp->prev = prev_temp; /* post_temp->prev ppint to prev_temp*/
+	prev_temp->next = post_temp; /* prev_temp-> next point to post_temp */
+	free(temp);
+	return (1); /*successful*/
+}
