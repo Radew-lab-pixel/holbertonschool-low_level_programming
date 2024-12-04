d16aea9cbb1dbc25a8f53f156387c7359c81737a
root <ktmqliu@gmail.com>
2024-12-04 11:28:39 +0000

    Amend 8-delete_dnodeint.c -- new from start - add out of range detection

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -66,7 +66,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	if (count > index) /* out of range */
+	if (count < index) /* out of range */
 	{
 		free(temp);
 		return (-1);
