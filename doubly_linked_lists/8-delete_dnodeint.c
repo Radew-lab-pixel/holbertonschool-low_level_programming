242ea0dc6e8f36a2f38f16f233294ae919893e0d
root <ktmqliu@gmail.com>
2024-12-04 02:26:50 +0000

    Backup

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *post_temp;
 	unsigned int count = 0;
 
-	if (*head == NULL)  /* added for checker */
+	if ((*head == NULL) || (index > INT_MAX)) /* added for checker */
 	{
 		free(*head);
 		return (-1);
