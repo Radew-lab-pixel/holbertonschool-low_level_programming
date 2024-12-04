a0c6552f3538569e11c137a62853c0a97e354a4c
root <ktmqliu@gmail.com>
2024-12-04 11:02:39 +0000

    Add new from scratch 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,15 +13,31 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp;
 	dlistint_t *prev_temp;
+	dlistint_t *next_temp;
 	unsigned int count = 0;
 
 	temp = *head;
 	while ((count < index) && (temp->next != NULL))
 	{
-		prev_temp = temp; 
+		prev_temp = temp;
 		temp = temp->next;
 		count++;
 	}
-
-
-
+	if (count == index) /* found index */
+	{
+		if (temp->next != NULL) /* not the last node */
+		{
+			next_temp = temp->next;
+			next_temp->prev = prev_temp;
+			prev_temp->next = next_temp;
+		}
+		else /* temp is last node */
+		{
+			prev_temp->next = NULL;			
+		}
+		temp = NULL;
+		free(temp);
+		return (1);
+	}
+	return (1);
+}
