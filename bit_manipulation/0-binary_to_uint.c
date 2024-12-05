4dc2723fce816cabf5620abc6a4f6881ecebd382
root <ktmqliu@gmail.com>
2024-12-05 14:22:43 +0000

    Backup

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-int _strlen(char *s);
-int binaryToInt(char *s, int length, int count);
+int _strlen(const char *s);
+int binaryToInt(int binaryInInt, int start);
+int _atoi(const char *s);
 
 /**
  * binary_to_unit -function to convert binary to decimal
@@ -13,9 +14,9 @@ int binaryToInt(char *s, int length, int count);
 unsigned int binary_to_uint(const char *b)
 {
 	const char *temp = b;
-	int length; 
+	/*int length; */ 
 
-	length = _strlen(temp);
+	/*length = _strlen(temp);*/
 	
 }
 
@@ -25,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
  * @s: string input
  * Return: int result of length
  */
-int _strlen(char *s)
+int _strlen(const char *s)
 {
 	int count = 0; 
 
@@ -41,16 +42,44 @@ int _strlen(char *s)
 }
 
 /**
- * binaryToInt - function to convert binary to Integer
+ * binaryToInt - function to convert binary numerical  to pure Integer
  *
- * @s: binary string input
- * @length: int of length of str
+ * @binaryInInt: binary result stored as Integer eg. int binaryInInt = 1101
+ * @start: start_position
  * Return: integer result of binary conversion
  */
-int binaryToInt(char *s, int length, int count)
+int binaryToInt(int binaryInInt, int start)
 {
+	int result = 0;
 	if (count == length)
 	{
-
+		return (s[i] - '0'); /* return MSB bit in integer format */
 	}
+	result = s;
+	binaryToInt(
+
+}
+
+/**
+ * _atoi - custom function converting string to integer
+ *
+ * @s : input
+ * Return: integer value of s
+ */
+int _atoi(const char *s)
+{
+	int result = 0;
+	int count = 0;
+
+	if (str[0] == -)
+	{
+		return (0); /* error as negative value detected */
+	}
+	while (s[count] != '\0')
+	{	/* go from right to left of s */
+		result = result * 10 + (s[count] - '0');
+		count++;
+	}
+	return (result);
+
 }
