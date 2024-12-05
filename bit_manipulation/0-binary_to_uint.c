55d4ab3514d51afc8c77fb0cb5666a4da17c449a
root <ktmqliu@gmail.com>
2024-12-05 06:13:35 +0000

    Backup

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * binary_to_unit -function to convert binary to decimal
+ *
+ * @b: string containing binary input
+ * Return: unsigned int result of converted value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	const char *temp = b;
+	int count = 0;
+}
+
+/**
+ * _strlen - function return length of str
+ *
+ * @s: string input
+ * Return: int result of length
+ */
+int _strlen(char *s)
+{
+	int count = 0; 
+
+	while (*str != '\0')
+	{
+		
+	}
+}
