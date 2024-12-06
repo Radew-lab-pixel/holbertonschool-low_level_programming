66877eb3228a53717425a21117e43f2d9c031d15
ktmqliu@gmail.com <ktmqliu@gmail.com>
2024-12-06 15:01:33 +0000

    Amend 2-get_bit.c betty

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,21 +11,22 @@ int getBitAtPosition(unsigned int num, unsigned int index);
  *
  * @n : integer input
  * @index : position of the bits
- * Return : bit value
+ * Return: binary digit if successful else -1
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int ntemp = n;
 	/*char binaryArray[33] = {0} ; 32 bits of unsigned int plus '\0' */
-	/* char *binaryArray = (char *)malloc(33); 32 bits of long int and plus '\0' */
+	/* char *binaryArray = (char *)malloc(33); */
+	/*32 bits of long int and plus '\0' */
 	/*int count = 0;  counter to keep track of char binaryArray */
 	int binaryDigit; /* binary value of the binaryArray[index] */
 
 	if ((ntemp == 0) || (index > 32))
 		return (-1);
 	if (intCheck(ntemp))
-	{	
+	{
 		binaryDigit =  getBitAtPosition(n, index);
 		/*decimalToBinary(ntemp, binaryArray); */
 
@@ -39,7 +40,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		/*printf("binaryDigit : %d\n", binaryDigit);*/
 		/*binary[(index)++] = (num & 1) + '0';*/
 
-		return (binaryDigit); 
+		return (binaryDigit);
 	}
 	/*return (binaryDigit); */
 	return (-1);
@@ -50,25 +51,25 @@ int get_bit(unsigned long int n, unsigned int index)
  *
  * @num : input
  * @index : position of the inteded bit
- * Return : bit
- */ 
+ * Return: bit
+ */
 
 int getBitAtPosition(unsigned int num, unsigned int index)
-{	
+{
 	int curLSB;
-        unsigned long int ntemp = num;
-	
+	unsigned long int ntemp = num;
+
 	ntemp = ntemp >> index;
 	curLSB = ntemp & 1;
 	return (curLSB);
 }
 
 
-/** decimalToBinary - function to convert decimal to binary and stored in array
+/**
+ * decimalToBinary - function to convert decimal to binary and stored in array
  *
  * @num : input
  * @binary : char array for storage
- * @count: position counter
  * Return : void
  */
 
@@ -78,7 +79,7 @@ void decimalToBinary(unsigned long int num, char binary[])
 	unsigned long int ntemp = num;
 	int count = 0;
 
-	if (ntemp == 0) 
+	if (ntemp == 0)
 	{
 		binary[0] = '0';
 		binary[1] = '\0';
@@ -134,8 +135,7 @@ void dec2BinaryArray(unsigned long int n, char s[], int *count)
 	/*binary[(*index)++] = (num & 1) + '0'; */
 	printf("s[*(count)] : %c\n", s[*(count)]);
 	/*return (s); */
-	return;
-}	
+}
 
 /**
  * intCheck - function to check if input integer is valid
