c9c3097f02180d203eb9b50318e5748cb0af73b1
root <ktmqliu@gmail.com>
2024-12-16 05:27:01 +0000

    Backup

--- simple_shell/function.c
+++ simple_shell/function.c
@@ -5,7 +5,7 @@ void print_prompt(void)
 {
 	if (isatty(STDIN_FILENO))
 	{
-		printf("simple_shell$ ");
+		printf("simple_shell$");
 		fflush(stdout);
 	}
 }
@@ -20,10 +20,29 @@ char *read_input(void)
 
 	if (read_command == -1)
 	{
+		
+		if (feof(stdin)) /* end of file */
+		{
+			free(input_line);
+			printf("End of file /Ctrl D detected.. Exiting\n");
+			exit (1);
+		}
+		else
+		{	
+			perror("Error in getline\n");	
+		}
+			
 		free(input_line);
 		return (NULL);
 	}
 
+	if (strcmp(input_line, "exit") == 0) /*exit */ 
+	{
+		/*free(input_line);*/
+		printf("Exiting\n");
+		exit (1);
+	}
+	
 	return (input_line);
 }
 
@@ -32,7 +51,7 @@ void trailing_input(char *input_trail)
 {
 	if (input_trail)
 	{
-		input_trail[strcspn(input_trail, "\n")] = 0;
+		input_trail[strcspn(input_trail, "\n")] = '\0';
 	}
 }
 
@@ -42,10 +61,24 @@ int command_handler(char *command)
 	pid_t PID;
 	int status;
 	char *args[2];
+	
+	char *token;
+	int count = 0;
 
 	/* Preparation arguments for EXECVE */
-	args[0] = command;
-	args[1] = NULL;
+	/*args[0] = command; */
+	/*args[1] = NULL; */
+
+	args[100];
+	
+	/* Tokenize */
+	token = strtok(command, " ");
+	while (token != NULL) 
+	{
+		args[count++] = token;   
+		token = strtok(NULL, " ");
+	}
+	args[count] = NULL;
 
 	if (!command || strlen(command) == 0)
 	{
@@ -61,17 +94,19 @@ int command_handler(char *command)
 	}
 	else if (PID == 0)
 	{
-		if (execve(command, args, environ) == -1)
+		/* if (execvp(command, args) == -1) */
+		if (execve(args[0], args, environ) == -1)
 		{
-			fprintf(stderr, "%s: Command not found in PATH\n", command);
+			fprintf(stderr, "%s: %s Command not found in PATH\n", command, strerror(errno));
 			exit(EXIT_FAILURE);
 		}
 	}
-		else
+	else
 		{
-			do {
+			do { 
 				waitpid(PID, &status, WUNTRACED);
 			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+			/*wait(NULL);*/
 		}
 	return (0);
 }
