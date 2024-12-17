3ae7e1cdd9224144c91baf4f11975e5f22b655ef
OEM Configuration (temporary user) <ktmqliu@gmail.com>
2024-12-17 12:59:56 +1100

    ammend 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,20 +24,20 @@ int main(int argc, char **argv)
 	fd = open(file_from, O_RDONLY, S_IRUSR | S_IRGRP | S_IROTH);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 /* fdo = open(file_to, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH); */
 	fdo = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 if (fdo == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 	bytesRead = read(fd, buffer, sizeof(buffer)); /* read input file */
 	if (bytesRead == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	while (bytesRead > 0) /*bytesRead from file > 0 */
@@ -45,26 +45,26 @@ if (fdo == -1)
 		bytesWritten = write(fdo, buffer, bytesRead);
 		if (bytesWritten == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 		bytesRead = read(fd, buffer, bytesRead);
 		if (bytesRead == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s", file_from);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
 	}
 	fclose = close(fd);
 	if (fclose == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	fclose = close(fdo);
 	if (fclose == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdo);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdo);
 		exit(100);
 	}
 	/*return (0);*/
